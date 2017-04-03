1800efc37d261f66453ae3e405ecd959b3f14f59
mcs2515 <mcs2515@rit.edu>
2017-04-02 23:51:49 -0400

    attempted jquery

--- js/reactions.js
+++ js/reactions.js
@@ -1,14 +1,9 @@
-$('.top').click(function() {      // When arrow is clicked
-        $('body,html').animate({
-            scrollTop : 0                       // Scroll to top of body
-        }, 500);
-     });
-
-     $(window).scroll(function() {
-        if ( $(window).scrollTop() > 450 ) {
-            $('.top').fadeIn('slow');
-        } 
-         else {
-            $('.top').fadeOut('slow');
-        }
-    });
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 'slow);
+    }
+});
