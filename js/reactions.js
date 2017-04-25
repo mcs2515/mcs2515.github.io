eba10748c35bc6aa73c46a9623c06c9563eccc84
Megan Smith (RIT Student) <mcs2515@ad.rit.edu>
2017-04-25 12:36:05 -0400

    used masonry layout for projects

--- js/reactions.js
+++ js/reactions.js
@@ -10,36 +10,36 @@ const init = () =>{
         }
     });
     
-        //fade top on scroll
-    $(window).scroll(function(){
-        
-        if ( $(window).scrollTop() > 50 ) {
-            BlurID("#about img", 40);
-        }
-        if ( $(window).scrollTop() > 120 ) {
-            BlurID("#about p", 100);
-        }
-        
-        if ( $(window).scrollTop() > 300 ) {
-            BlurID("#goal h2", 200);
-        }
-        
-        if ( $(window).scrollTop() > 350 ) {
-
-            BlurID("#goal h3", 200);
-        }
-        
-        if ( $(window).scrollTop() > 390 ) {
-            BlurID("#goal p", 200);
-        }
-
-  });
-    
-    function BlurID(sec, pixels){
-        var pixs = $(document).scrollTop()
-        pixs = pixs / pixels;
-        $(sec).css({"-webkit-filter": "blur("+pixs+"px)","filter": "blur("+pixs+"px)" })
-    }
+//        //fade top on scroll
+//    $(window).scroll(function(){
+//        
+//        if ( $(window).scrollTop() > 50 ) {
+//            BlurID("#about img", 40);
+//        }
+//        if ( $(window).scrollTop() > 120 ) {
+//            BlurID("#about p", 100);
+//        }
+//        
+//        if ( $(window).scrollTop() > 300 ) {
+//            BlurID("#goal h2", 200);
+//        }
+//        
+//        if ( $(window).scrollTop() > 350 ) {
+//
+//            BlurID("#goal h3", 200);
+//        }
+//        
+//        if ( $(window).scrollTop() > 390 ) {
+//            BlurID("#goal p", 200);
+//        }
+//
+//  });
+//    
+//    function BlurID(sec, pixels){
+//        var pixs = $(document).scrollTop()
+//        pixs = pixs / pixels;
+//        $(sec).css({"-webkit-filter": "blur("+pixs+"px)","filter": "blur("+pixs+"px)" })
+//    }
 }
 
 window.onload=init;
