f47f59d69a72b02219f36a6c2b677b2300605e20
Megan Smith (RIT Student) <mcs2515@ad.rit.edu>
2018-09-20 17:41:02 -0400

    moved js to own fold. added slick

---
+++ js/index.js
@@ -0,0 +1,30 @@
+(function(){
+	"use strict";
+	
+	function init(){
+		$("a[href='#top']").click(function() {
+			$("html, body").animate({ scrollTop: 0 }, "slow");
+			return false;
+		});
+		
+		$("a[href='#projectSection']").click(function() {
+			$("html, body").animate({ scrollTop: 1050 }, "slow");
+			return false;
+		});
+		
+		$("a[href='#copyright']").click(function() {
+			$("html, body").animate({ scrollTop: $(document).height()}, "slow");
+			return false;
+		});
+		
+		$('.carousel').slick({
+		  slidesToShow: 3,
+		  slidesToScroll: 1,
+		  autoplay: true,
+		  autoplaySpeed: 2000,
+		  dots: true,
+		});
+	}
+	
+	window.onload = init;
+})();
