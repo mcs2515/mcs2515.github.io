25bbc085cdeaf576ff808ebe0315b76d8367f573
mcs2515 <mcs2515@rit.edu>
2018-09-22 22:14:26 -0400

    color changes and major tweaks

--- js/index.js
+++ js/index.js
@@ -18,11 +18,13 @@
 		});
 		
 		$('.carousel').slick({
-		  slidesToShow: 3,
-		  slidesToScroll: 1,
-		  autoplay: true,
-		  autoplaySpeed: 2000,
-		  dots: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        centerMode: true,
+        centerPadding: '60px',
 		});
 	}
 	
