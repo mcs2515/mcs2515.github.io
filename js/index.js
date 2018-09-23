b17f17ce950bbe7e055657147776041671ef4c6f
mcs2515 <mcs2515@rit.edu>
2018-09-23 01:21:04 -0400

    added background image and parallax

--- js/index.js
+++ js/index.js
@@ -1,32 +1,38 @@
 (function(){
-	"use strict";
-	
-	function init(){
-		$("a[href='#top']").click(function() {
-			$("html, body").animate({ scrollTop: 0 }, "slow");
-			return false;
-		});
-		
-		$("a[href='#projectSection']").click(function() {
-			$("html, body").animate({ scrollTop: 1050 }, "slow");
-			return false;
-		});
-		
-		$("a[href='#copyright']").click(function() {
-			$("html, body").animate({ scrollTop: $(document).height()}, "slow");
-			return false;
-		});
-		
-		$('.carousel').slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        dots: true,
-        centerMode: true,
-        centerPadding: '60px',
-		});
-	}
-	
-	window.onload = init;
+    
+    "use strict";
+    
+    function init(){
+        $("a[href='#top']").click(function() {
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+            return false;
+        });
+        
+        $("a[href='#projectSection']").click(function() {
+            $("html, body").animate({ scrollTop: 1050 }, "slow");
+          return false;
+        });
+        
+        $("a[href='#copyright']").click(function() {
+            $("html, body").animate({ scrollTop: $(document).height()}, "slow");
+            return false;
+        });
+        
+        $(".about, [data-paroller-factor]").paroller({
+            factor: .5,
+            type: 'foreground',
+        });
+        
+        
+        $('.carousel').slick({
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            dots: true,
+            centerMode: true,
+        });
+    }
+    
+    window.onload = init;
 })();
